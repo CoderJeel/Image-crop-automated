871bf4ac0b9b7c6aeadcd0f7514e0881b5dd4204
Kevan Hingarajiya <kevanhingarajiya19@gmail.com>
2021-06-11 14:57:13 +0530

    final one without link social media

--- webpage/script.js
+++ webpage/script.js
@@ -1,67 +1,67 @@
 
-      /**
-       * for upload-box
-       */
-      
-       const wrapper = document.querySelector(".wrapper");
-       const fileName = document.querySelector(".file-name");
-       const defaultBtn = document.querySelector("#default-btn");
-       const customBtn = document.querySelector("#custom-btn");
-       const cancelBtn = document.querySelector("#cancel-btn i");
-       const img = document.querySelector("img");
-       let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
-       function defaultBtnActive() {
-         defaultBtn.click();
-       }
-       defaultBtn.addEventListener("change", function () {
-         const file = this.files[0];
-         if (file) {
-           const reader = new FileReader();
-           reader.onload = function () {
-             const result = reader.result;
-             img.src = result;
-             wrapper.classList.add("active");
-           };
-           cancelBtn.addEventListener("click", function () {
-             img.src = "";
-             wrapper.classList.remove("active");
-           });
-           reader.readAsDataURL(file);
-         }
-         if (this.value) {
-           let valueStore = this.value.match(regExp);
-           fileName.textContent = valueStore;
-         }
-       });
-     
-       /**for side bar and effect */
- 
-       function toggle() {
-         const sidebar = document.querySelector(".sidebar");
-         const main = document.querySelector(".main");
-       
-         sidebar.classList.toggle("active");
-         main.classList.toggle("active");
-       }
-       
-       /* Ignore this below */
-       /* It just sets the icon svgs */
-       /* Kept it here to keep the HTML clean */
-       function onLoad() {
-         const hamburger = document.querySelector(".hamburger");
-       
-         hamburger.innerHTML = hamburgerSVG();
-       
-         const github = document.querySelector(".github");
-         const color = "#231f20";
-       
-         github.innerHTML = githubLogo(color);
-       }
-       
-       window.addEventListener("load", onLoad);
-       
-       function githubLogo(color) {
-         return `<?xml version="1.0" encoding="utf-8"?>
+/**
+ * for upload-box
+ */
+
+const wrapper = document.querySelector(".wrapper");
+const fileName = document.querySelector(".file-name");
+const defaultBtn = document.querySelector("#default-btn");
+const customBtn = document.querySelector("#custom-btn");
+const cancelBtn = document.querySelector("#cancel-btn i");
+const img = document.querySelector("img");
+let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
+function defaultBtnActive() {
+  defaultBtn.click();
+}
+defaultBtn.addEventListener("change", function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function () {
+      const result = reader.result;
+      img.src = result;
+      wrapper.classList.add("active");
+    };
+    cancelBtn.addEventListener("click", function () {
+      img.src = "";
+      wrapper.classList.remove("active");
+    });
+    reader.readAsDataURL(file);
+  }
+  if (this.value) {
+    let valueStore = this.value.match(regExp);
+    fileName.textContent = valueStore;
+  }
+});
+
+/**for side bar and effect */
+
+function toggle() {
+  const sidebar = document.querySelector(".sidebar");
+  const main = document.querySelector(".main");
+
+  sidebar.classList.toggle("active");
+  main.classList.toggle("active");
+}
+
+/* Ignore this below */
+/* It just sets the icon svgs */
+/* Kept it here to keep the HTML clean */
+function onLoad() {
+  const hamburger = document.querySelector(".hamburger");
+
+  hamburger.innerHTML = hamburgerSVG();
+
+  const github = document.querySelector(".github");
+  const color = "#231f20";
+
+  github.innerHTML = githubLogo(color);
+}
+
+window.addEventListener("load", onLoad);
+
+function githubLogo(color) {
+  return `<?xml version="1.0" encoding="utf-8"?>
        <!-- Generator: Adobe Illustrator 25.2.3, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
             viewBox="0 0 32.58 31.77" style="enable-background:new 0 0 32.58 31.77;" xml:space="preserve" fill="#231f20">
@@ -77,7 +77,7 @@
            c6.47-2.16,11.13-8.26,11.13-15.45C32.58,7.29,25.29,0,16.29,0z"/>
        </svg>
        `;
-       }
-       function hamburgerSVG() {
-         return `<svg height='50px' width='50px'  fill="#231f20" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" version="1.1" x="0px" y="0px"><title>hamburger-menu</title><desc>Created with Sketch.</desc><g stroke="none" stroke-width="1" fill="#231f20" fill-rule="evenodd"><g fill="#231f20"><path d="M5,6 L19,6 C19.5522847,6 20,6.44771525 20,7 C20,7.55228475 19.5522847,8 19,8 L5,8 C4.44771525,8 4,7.55228475 4,7 C4,6.44771525 4.44771525,6 5,6 Z M5,11 L19,11 C19.5522847,11 20,11.4477153 20,12 C20,12.5522847 19.5522847,13 19,13 L5,13 C4.44771525,13 4,12.5522847 4,12 C4,11.4477153 4.44771525,11 5,11 Z M5,16 L19,16 C19.5522847,16 20,16.4477153 20,17 C20,17.5522847 19.5522847,18 19,18 L5,18 C4.44771525,18 4,17.5522847 4,17 C4,16.4477153 4.44771525,16 5,16 Z"></path></g></g></svg>`;
-       }
+}
+function hamburgerSVG() {
+  return `<svg height='50px' width='50px'  fill="#231f20" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" version="1.1" x="0px" y="0px"><title>hamburger-menu</title><desc>Created with Sketch.</desc><g stroke="none" stroke-width="1" fill="#231f20" fill-rule="evenodd"><g fill="#231f20"><path d="M5,6 L19,6 C19.5522847,6 20,6.44771525 20,7 C20,7.55228475 19.5522847,8 19,8 L5,8 C4.44771525,8 4,7.55228475 4,7 C4,6.44771525 4.44771525,6 5,6 Z M5,11 L19,11 C19.5522847,11 20,11.4477153 20,12 C20,12.5522847 19.5522847,13 19,13 L5,13 C4.44771525,13 4,12.5522847 4,12 C4,11.4477153 4.44771525,11 5,11 Z M5,16 L19,16 C19.5522847,16 20,16.4477153 20,17 C20,17.5522847 19.5522847,18 19,18 L5,18 C4.44771525,18 4,17.5522847 4,17 C4,16.4477153 4.44771525,16 5,16 Z"></path></g></g></svg>`;
+}
